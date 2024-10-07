0a7c13caca03cde010146c08bbdda31e28cdc753
ArrogantGao <gaoxuanzhao@gmail.com>
2024-10-07 18:36:12 +0800

    update

--- examples/FET.jl
+++ examples/FET.jl
@@ -1,4 +1,6 @@
-using Plots, SumOfExpVPMR
+using SumOfExpVPMR
+using CairoMakie, LaTeXStrings
+using BenchmarkTools
 
 function direct_sum(f::Function, q_1::Vector{T}, q_2::Vector{T}, x::Vector{T}) where{T}
     sum_result = zero(T)
@@ -20,11 +22,6 @@ begin
     time_direct = Float64[]
     time_FET = Float64[]
 
-    # warm up
-    direct_sum(f, rand(2), rand(2), rand(2))
-    FET1d(rand(2), rand(2), rand(2), soepara)
-
-
     N_array = [100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600]
     for N in N_array
         q_1 = rand(N)
@@ -33,14 +30,22 @@ begin
 
         sort_x = sortperm(x)
 
-        push!(time_direct, @elapsed direct_sum(f, q_1, q_2, x))
-        push!(time_FET, @elapsed FET1d(q_1, q_2, x, soepara, sort_x = sort_x))
+        push!(time_direct, @belapsed direct_sum($f, $q_1, $q_2, $x))
+        push!(time_FET, @belapsed FET1d($q_1, $q_2, $x, $soepara, sort_x = $sort_x))
+        @show N, time_direct[end], time_FET[end]
     end
 end
 
 begin
-    plot(xlabel = "log(N)", ylabel = "log(time)", dpi = 500, title = "Compare direct sum and FET")
-    plot!(log10.(N_array), log10.(time_direct), label = "direct sum", marker = :circle)
-    plot!(log10.(N_array), log10.(time_FET), label = "FET", marker = :square)
-    savefig("FET.png")
+    fig = Figure()
+    ax = Axis(fig[1, 1]; xlabel = L"N", ylabel = "time (s)", yscale = log10, xscale = log10)
+    scatter!(ax, N_array, time_direct, label = "Direct sum", color = :blue, markersize = 15, marker = :diamond)
+    scatter!(ax, N_array, time_FET, label = "FGT", color = :red, markersize = 15, marker = :utriangle)
+    x_array = [50:100:25600 * 2...]
+    lines!(ax, x_array, x_array.^2 .* (time_direct[end] / N_array[end]^2) , color = :blue, linestyle = :dash)
+    lines!(ax, x_array, x_array .* (time_FET[end] / N_array[end]), color = :red, linestyle = :dash)
+    axislegend(ax, position = :lt)
+    xlims!(ax, 100 * 0.75, 25600 / 0.75)
+    fig
+    save("FET.png", fig, px_per_unit = 2)
 end
